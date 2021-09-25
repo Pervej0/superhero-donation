adefd0c2d00a46e684485a0cb17da31ce35d5848
Pervej0 <mdparvez222khan@gmail.com>
2021-09-26 00:16:14 +0600

    Header part added

--- src/App.js
+++ src/App.js
@@ -1,11 +1,13 @@
 import "./App.css";
+import React from "react";
 import "bootstrap/dist/css/bootstrap.min.css";
 import "bootstrap/dist/js/bootstrap.min";
+import Header from "./Components/Header/Header";
 
 function App() {
   return (
     <div className="App">
-      <h1>Hello world</h1>
+      <Header />
     </div>
   );
 }
